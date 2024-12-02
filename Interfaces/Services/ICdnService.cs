b418f58377110fcc84d464f92d5940a0b71eb030
xsjens02 <simjen02@easv365.dk>
2024-12-02 20:53:30 +0100

    added CdnService

---
+++ Interfaces/Services/ICdnService.cs
@@ -0,0 +1,8 @@
+﻿namespace ResourceBookingAPI.Interfaces.Services
+{
+    public interface ICdnService
+    {
+        Task<string> UploadFile(IFormFile file);
+        Task<bool> DeleteFile(string url);
+    }
+}
---
+++ Services/CdnService.cs
@@ -0,0 +1,17 @@
+﻿using ResourceBookingAPI.Interfaces.Services;
+
+namespace ResourceBookingAPI.Services
+{
+    public class CdnService : ICdnService
+    {
+        public Task<bool> DeleteFile(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UploadFile(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
